03d8a11a3192d570241e6a8592c87d1d25c0dec9
Mary78234 <wang11quanquan@gmail.com>
2021-04-27 11:38:47 +0200

    snack1

---
+++ JSnack1/script.js
@@ -0,0 +1,67 @@
+/* 
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+Calcolca qunto pesano tutte le zucchine.
+*/
+
+//crea array di oggetti zucchine
+var zucchine = [
+  {
+    varieta: "nere di Milano",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "bianche di Trieste",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "nere di Milano",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "nere di Milano",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "striate del centro",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "nere di Milano",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "striate del centro",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "bianche di Trieste",
+    peso: 20,
+    lunghezza: 35,
+  },
+  {
+    varieta: "bianche di Trieste",
+    peso: 25,
+    lunghezza: 35,
+  },
+  {
+    varieta: "striate del centro",
+    peso: 20,
+    lunghezza: 35,
+  },
+]
+
+var pesoTot = 0;
+
+//calcola peso delle zucchine
+for (var key in zucchine){
+  pesoTot += zucchine[key].peso;
+}
+
+console.log("Peso totale zucchine " + pesoTot + "g.")
